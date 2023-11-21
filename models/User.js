724f80e0fe9dd845295c9939992cec170124a5c9
Janet Webster <twixmixy@gmail.com>
2023-11-20 22:48:52 -0500

    Trying to build user schema

---
+++ models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        }
+    }
+)
