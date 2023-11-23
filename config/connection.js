62e94d44904263a3d54d197d6ade1ed90384b8a3
Janet Webster <twixmixy@gmail.com>
2023-11-23 17:36:40 -0500

    Adding comments to the connection file

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,12 @@
+// IMPORT the Mongoose library to use MongoDB
 const mongoose = require('mongoose');
 
+// Establishing the connection to the MongoDB database. Giving the option to use the default connection string. Added options to ensure compatibility with newer versions of MongoDB.
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialnetwork_db', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
 
+// EXPORT the connection object. Allowing other modules in the application to access and interact with it.
 module.exports = mongoose.connection;
