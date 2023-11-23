a48c39495821d9a4b17837937a55b507da8fd7c7
Janet Webster <twixmixy@gmail.com>
2023-11-22 22:12:31 -0500

    Built out User schema

--- models/User.js
+++ models/User.js
@@ -17,8 +17,28 @@ const UserSchema = new Schema(
         thoughts: [
             {
                 type: Schema.Types.ObjectId,
-                ref:  "Thought"
+                ref:  "Thought",
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "User",
             }
         ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
     }
-)
+);
+
+UserSchema.virtual("friendCount").get(function() {
+    return this.friends.length;
+});
+
+const User = model("User", UserSchema);
+
+module.exports = User;
