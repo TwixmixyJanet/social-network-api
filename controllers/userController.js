3877b803448d53f62f3f2010d2c6e0c3760f5a5e
Janet Webster <twixmixy@gmail.com>
2023-11-22 23:14:44 -0500

    Built out userControllers for relationships

---
+++ controllers/userController.js
@@ -0,0 +1,118 @@
+const { User, Thought } = require("../models");
+
+const userController = {
+    getUsers(req, res) {
+        User.find()
+        .select("-__v")
+        .then((dbUserData) => {
+            res.json(dbUserData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+    
+    getSingleUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+        .select("-__v")
+        .then((dbUserData) => {
+            if (!dbUserData) {
+                return res.status(404).json({ message: "No user with this ID" });
+            }
+            res.json(dbUserData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+
+    createUser(req, res) {
+        User.create(req.body)
+        .then((dbUserData) => {
+            res.json(dbUserData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+
+    updateUser(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body, },
+            {
+                runValidators: true,
+                new: true,
+            }
+        )
+        .then((dbUserData) => {
+            if (!dbUserData) {
+                return res.status(404).json({ message: "No user with this ID" });
+            }
+            res.json(dbUserData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+
+    deleteUser(req, res) {
+        User.findOneAndDelete({ _id: req.params.userId })
+        .then((dbUserData) => {
+            if (!dbUserData) {
+                return res.status(404).json({ message: "No user with this ID" });
+            }
+            // BONUS to delete all of users thoughts
+            return Thought.deleteMany({ _id: { $in: dbUserData.thoughts }});
+        })
+        .then(() => {
+            res.json({ message: "The user and their thoughts are deleted"});
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+
+    addFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: { friends: req.params.friendId } },
+            { new: true }
+        )
+        .then((dbUserData) => {
+            if (!dbUserData) {
+                return res.status(404).json({ message: "No user with this ID" });
+            }
+            res.json(dbUserData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+
+    removeFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friends: req.params.friendId } },
+            { new: true }
+        )
+        .then((dbUserData) => {
+            if (!dbUserData) {
+                return res.status(404).json({ message: "No user with this ID" });
+            }
+            res.json(dbUserData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+};
+
+module.exports = userController;
