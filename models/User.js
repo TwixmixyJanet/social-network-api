afb4d3ab2e85f69651c295abc931e11e8e804f66
Janet Webster <twixmixy@gmail.com>
2023-11-24 11:59:24 -0500

    Added comments to user schema

--- models/User.js
+++ models/User.js
@@ -1,25 +1,33 @@
+// IMPORTS
+// using the mongoose library, pulling in the Schema constructor and the model function
 const { Schema, model } = require('mongoose');
 
+// New schema to manage users
 const UserSchema = new Schema(
     {
+        // Stores the user name
         username: {
             type: String,
             required: true,
             unique: true,
             trim: true,
         },
+        // Stores the user email
         email: {
             type: String,
             required: true,
             unique: true,
+            // Special mongoose regex matching for email
             match: [/.+\@.+\..+/, "Must match an email address format."]
         },
+        // Stores the array of thoughts
         thoughts: [
             {
                 type: Schema.Types.ObjectId,
                 ref:  "Thought",
             }
         ],
+        // Stores the array of friends
         friends: [
             {
                 type: Schema.Types.ObjectId,
@@ -28,6 +36,7 @@ const UserSchema = new Schema(
         ]
     },
     {
+        // getter applied when converting the document to JSON format
         toJSON: {
             virtuals: true,
         },
@@ -35,10 +44,13 @@ const UserSchema = new Schema(
     }
 );
 
+// This is to count the amount of friends per user. The virtual calculates it. By accessing the friends array and returning it's length.
 UserSchema.virtual("friendCount").get(function() {
     return this.friends.length;
 });
 
+// Using model to call on the userSchema
 const User = model("User", UserSchema);
 
+// EXPORT
 module.exports = User;
