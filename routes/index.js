7aea8e608df433a2c918905f78bdba6cc2ae50d2
Janet Webster <twixmixy@gmail.com>
2023-11-24 12:07:22 -0500

    Added comments to the main route index

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
+// IMPORTS
+// initializing the express router object
 const router = require('express').Router();
+// connection to the api routes
 const apiRoutes = require('./api');
 
+// mounting the apiRoutes router
 router.use('/api', apiRoutes);
 
+// catch-all middleware for any previously defined routes that don't match
 router.use((req, res) => {
     return res.send('<h1>❌ Wrong Route! ❌</h1>')
 });
 
+// EXPORT
 module.exports = router;
