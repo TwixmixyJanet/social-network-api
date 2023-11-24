1147d2b43c83ec36f11a43523f3e4c95d2e5eeed
Janet Webster <twixmixy@gmail.com>
2023-11-24 12:14:38 -0500

    Adding comments to the thought and reaction routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,4 +1,7 @@
+// IMPORTS
+// initializing the express router object
 const router = require('express').Router();
+// connection to all the thought and reaction functions in the thought controller
 const {
     getThoughts,
     getSingleThought,
@@ -9,12 +12,17 @@ const {
     removeReaction,
 } = require('../../controllers/thoughtController');
 
+// This route manages the GET and POST for retrieving all the thoughts and posting a new thought
 router.route('/').get(getThoughts).post(createThought);
 
+// This route manages the GET, PUT, and DELETE for getting a single thought, updating a thought, and deleting a thought
 router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
+// This route manages the POST for updating a thought and adding a reaction to it
 router.route('/:thoughtId/reactions').post(addReaction);
 
+// This route manages the DELETE for updating a thought and removing a reaction to it
 router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
 
+// EXPORT
 module.exports = router;
