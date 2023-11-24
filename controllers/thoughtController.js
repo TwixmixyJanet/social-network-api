6b19978c8aff75cefbdba3e5885e105ac9555b85
Janet Webster <twixmixy@gmail.com>
2023-11-23 21:52:29 -0500

    Adding comments to the thought controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,8 +1,13 @@
+// IMPORT to pull in the Thought and User models
 const { Thought, User } = require("../models");
 
+// This variable manages all of the needed thought controllers to be used by the routes
 const thoughtController = {
+    // GET all thoughts to retrieve them from the database and send them back as JSON data to the client.
     getThoughts(req, res) {
+        // Retrieve all thoughts from database
         Thought.find()
+        // Used to sort the thoughts by the createdAt timestamp in descending order, so the most recent thoughts display first
         .sort({ createdAt: -1 })
         .then((dbThoughtData) => {
             res.json(dbThoughtData)
@@ -13,7 +18,9 @@ const thoughtController = {
         });
     },
 
+    // GET a thought by ID
     getSingleThought(req, res) {
+        // If the thought is found, the data is returned and sent back to the client as JSON data. If not, it returns an error message.
         Thought.findOne({ _id: req.params.thoughtId })
         .then((dbThoughtData) => {
             if (!dbThoughtData) {
@@ -27,8 +34,11 @@ const thoughtController = {
         });
     },
 
+    // POST a new thought and update it to the associated user profile
     createThought(req, res) {
+        // This simplified syntax manages the thought creation process
         Thought.create(req.body)
+        // Update the data to the associated user, pushing the new thought. 
         .then((dbThoughtData) => {
             return User.findOneAndUpdate(
                 { _id: req.body.userId },
@@ -48,10 +58,13 @@ const thoughtController = {
         });
     },
 
+    // PUT update a thought
     updateThought(req, res) {
+        // find the existing thought via ID and update it using the set property
         Thought.findOneAndUpdate(
             { _id: req.params.thoughtId},
             { $set: req.body },
+            // ensures that the updated thought data adheres to the defined validation rules
             { runValidators: true, new: true }
         )
         .then((dbThoughtData) => {
@@ -66,7 +79,9 @@ const thoughtController = {
         });
     },
 
+    // DELETE thought by ID
     deleteThought(req, res) {
+        // Based off of the ID the function finds and deletes the associated thought
         Thought.findOneAndDelete({ _id: req.params.thoughtId })
         .then((dbThoughtData) => {
             if (!dbThoughtData) {
@@ -74,6 +89,7 @@ const thoughtController = {
             }
 
             // BONUS remove thought from user
+            // Updates the user to no longer have the deleted thought associated
             return User.findOneAndUpdate(
                 { thoughts: req.params.thoughtId },
                 { $pull: { thoughts: req.params.thoughtId } },
@@ -92,9 +108,12 @@ const thoughtController = {
         });
     },
 
+    // POST reaction by updating thought through ID
     addReaction(req, res) {
         Thought.findOneAndUpdate(
+            // finding and updating the thought by ID
             { _id: req.params.thoughtId },
+            // adds the reaction data to the array using addToSet, ensuring duplicates are not added.
             { $addToSet: { reactions: req.body } },
             { runValidators: true, new: true }
         )
@@ -110,9 +129,12 @@ const thoughtController = {
         });
     },
 
+    // DELETE reaction by updating thought through ID
     removeReaction(req, res) {
         Thought.findOneAndUpdate(
+            // same as add
             { _id: req.params.thoughtId },
+            // removed the reaction with the matching reaction ID from the array using pull. This ensures only the specified reaction is removed.
             { $pull: { reactions: { reactionId: req.params.reactionId } } },
             { runValidators: true, new: true }
         )
