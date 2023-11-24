3b484a03c03833a1ce59ee9e4a978aa4ecc159b1
Janet Webster <twixmixy@gmail.com>
2023-11-24 12:17:26 -0500

    Adding comments to user and friend routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,4 +1,7 @@
+// IMPORTS
+// initializing the express router object
 const router = require('express').Router();
+// connection to all the user and friend functions in the user controller
 const {
     getUsers,
     getSingleUser,
@@ -9,10 +12,14 @@ const {
     removeFriend,
 } = require('../../controllers/userController');
 
+// This route manages the GET and POST for retrieving all the users and posting a new user
 router.route('/').get(getUsers).post(createUser);
 
+// This route manages the GET, PUT, and DELETE for getting a single user, updating a user, and deleting a user
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
+// This route manages the POST and DELETE for adding and removing a user(friend) to a user
 router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
 
+// EXPORT
 module.exports = router;
