bba3d92b3dc523f571f2fc983829dfbc8cba70a5
Janet Webster <twixmixy@gmail.com>
2023-11-24 12:09:35 -0500

    Add comments to api route index

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,14 @@
+// IMPORTS
+// initializing the express router object
 const router = require('express').Router();
+// connection to the user routes
 const userRoutes = require('./userRoutes');
+// connection to the thought routes
 const thoughtRoutes = require('./thoughtRoutes');
 
+// mounting user and thought routes
 router.use('/users', userRoutes);
 router.use('/thoughts', thoughtRoutes);
 
+// EXPORT
 module.exports = router;
